671e42d821b7fa6a50c60cf8e839c5e2b0c3a795
Ayumu AIZAWA <ayumu.aizawa@gmail.com>
2017-10-14 20:45:39 +0900

    post twitter stream into Kafka

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var util = require('util');
 var twitter = require('ntwitter');
+var Kafka = require('no-kafka');
 
-// process.on('uncaughtException', function(e) {
-//   console.debug(e);
-// });
 
 var twit = new twitter({
   consumer_key: process.env.CONSUMER_KEY,
@@ -26,10 +24,26 @@ io.sockets.on('connection', function(socket) {
   })
 });
 
+var producer = new Kafka.Producer({
+  connectionString: process.env.KAFKA_URL,
+  ssl: {
+    cert: process.env.KAFKA_CLIENT_CERT || '.ssl/client.crt',
+    key: process.env.KAFKA_CLIENT_CERT_KEY || '.ssl/client.key'
+  }
+});
+producer.init();
+
 twit.stream('statuses/sample', function(stream) {
   stream.on('data', function (data) {
     if(data.lang === 'ja') {
       io.sockets.emit('msg', data);
+      producer.send({
+        topic: process.env.KAFKA_PREFIX + process.env.KAFKA_TOPIC,
+        partition: 0,
+        message: {
+          value: data.text
+        }
+      });
       if(process.env.DRAIN_TWEETS === '1') {
         console.log(data);
       }
